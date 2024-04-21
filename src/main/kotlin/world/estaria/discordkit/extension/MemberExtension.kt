a2469ef12c6cfc0a4471413dcb61214fae97d0c5
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-04-21 19:59:37 +0200

    add Members extension

--- src/main/kotlin/world/estaria/discordkit/extension/MemberExtension.kt
+++ src/main/kotlin/world/estaria/discordkit/extension/MemberExtension.kt
@@ -1,9 +1,15 @@
 package world.estaria.discordkit.extension
 
+import net.dv8tion.jda.api.entities.Member
+
 /**
  * @author Niklas Nieberler
  */
- 
-fun hasRole() {
 
+/**
+ * @param id of the role
+ * @return true if this player has this role
+ */
+fun Member.hasRole(id: String): Boolean {
+    return this.roles.any { it.id == id }
 }
